bec040775c2204ccff9e4fd95a03c608503419d9
Daniil <dadyarri@gmail.com>
2022-12-10 12:23:33 +0300

    add data for header links

---
+++ components/header/header.data.ts
@@ -0,0 +1,20 @@
+const data = [
+    {
+        "path": "home",
+        "label": "Главная"
+    },
+    {
+        "path": "features",
+        "label": "Функционал"
+    },
+    {
+        "path": "pricing",
+        "label": "Цены"
+    },
+    {
+        "path": "preorder",
+        "label": "Предзаказ"
+    }
+]
+
+export default data;
