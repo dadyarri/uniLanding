81e268ecb7fc2ef547d189f1ca19502647c66439
Daniil <dadyarri@gmail.com>
2022-12-10 12:46:36 +0300

    load theme

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,17 @@
 import Head from "next/head";
 import Header from "../components/header/header";
 import React from "react";
+import {ThemeProvider} from "theme-ui";
+import theme from "../theme";
 
 export default function Home() {
     return (
-        <>
+        // @ts-ignore
+        <ThemeProvider theme={theme}>
             <Head>
                 <title>UNI</title>
             </Head>
             <Header></Header>
-        </>
+        </ThemeProvider>
     )
 }
