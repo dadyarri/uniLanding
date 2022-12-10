1cc9eacc279e3489aa5e5f45e0ea12ba063792a6
Daniil <dadyarri@gmail.com>
2022-12-10 13:56:35 +0300

    disable image optimize
    for some fucking reason it doesn't work on vercel

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-const withPlugins = require("next-compose-plugins");
-const optimizedImages = require("next-optimized-images");
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 };
 
-module.exports = withPlugins([optimizedImages], nextConfig);
+module.exports = nextConfig;
